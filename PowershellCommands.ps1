7ed6dd179a5b026aa66f679d8f8b0d279e33d296
Daniel Nitz <daniel.nitz@datef.it>
2017-02-09 11:06:20 +0100

    Initial

---
+++ PowershellCommands.ps1
@@ -0,0 +1,8 @@
+﻿# Connect to Azure RM Account
+Login-AzureRmAccount
+
+# Select Azure RM Subscription
+Select-AzureRmSubscription -SubscriptionId c2a12a42-0179-404f-ad43-b8690de370fd
+
+# ARM Template Create virtual Network 101-vnet-two-subnets
+New-AzureRmResourceGroupDeployment -Name TestVNetDeploxment -ResourceGroupName rg_Arm -TemplateUri https://github.com/Azure/azure-quickstart-templates/blob/master/101-vnet-two-subnets/azuredeploy.json -TemplateParameterFile F:\azuredeploy-parameters.json
